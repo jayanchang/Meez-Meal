9fb8151835f026efc66f2d71a8a630587692851c
Shunimelb <fukui@student.unimelb.edu.au>
2024-05-28 13:02:52 +1000

    aaa

---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+const slidesContainer = document.getElementById("carousel");
+const slide = document.querySelector(".carousel-item");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
