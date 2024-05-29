7ab10783e78edb3d7ca1ee641b7f89470e3513e5
Shunimelb <fukui@student.unimelb.edu.au>
2024-05-30 00:09:06 +1000

    basic done

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,30 +1,3 @@
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-
-one.onclick = function() {
-    one.classList.add("active");
-    two.classList.remove("active");
-    three.classList.remove("active");
-    four.classList.remove("active");
-    five.classList.remove("active");
-}
-
-two.onclick = function() {
-    one.classList.add("active");
-    two.classList.add("active");
-    three.classList.remove("active");
-    four.classList.remove("active");
-    five.classList.remove("active");
-}
-three.onclick = function() {
-    one.classList.add("active");
-    two.classList.add("active");
-    three.classList.add("active");
-    four.classList.remove("active");
-    five.classList.remove("active");
-}
-
 let count = 1;
 let countup_btn = document.getElementById("countup");
 let countdown_btn = document.getElementById("countdown")
