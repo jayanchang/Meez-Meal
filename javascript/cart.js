21ccce83361e2bfe59743c3806510add610a18ff
Shunimelb <fukui@student.unimelb.edu.au>
2024-05-30 21:50:42 +1000

    finalisation (2) - shun

--- javascript/cart.js
+++ javascript/cart.js
@@ -3,13 +3,27 @@ let countup_btn = document.getElementById("countup");
 let countdown_btn = document.getElementById("countdown")
 let disp = document.getElementById("count");
 
+let price = 15.5;
+let changed_price = 0;
+
 countup_btn.addEventListener("click", function () {
     count++;
+    changed_price = price * count;
     disp.innerHTML = count;
+    document.getElementById("price").textContent = changed_price;
+    document.getElementById("tot-price-text").textContent = changed_price;
 });
 
 countdown_btn.addEventListener("click", function (){
-    count--;
-    disp.innerHTML = count;
+    if (count == 0){
+        return;
+    } else{
+        count--;
+        changed_price = price * count;
+        disp.innerHTML = count;
+        document.getElementById("price").textContent = changed_price;
+        document.getElementById("tot-price-text").textContent = changed_price;
+    }
 });
+
 
